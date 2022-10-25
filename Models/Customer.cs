94c2f1487396d96fcd31929813bd7b6009eb326c
deibysch <pedrodeiby@gmail.com>
2022-10-25 15:24:40 -0400

    cambios de deiby

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace WebApplicationNW.Models
 {
     public partial class Customer
     {
+        public static List<PropertyInfo> Columns = CargarColumnas();
+        private static List<PropertyInfo> CargarColumnas()
+        {
+            List<PropertyInfo> c = new List<PropertyInfo>();
+            foreach (PropertyInfo column in typeof(Customer).GetProperties())
+            {
+                string nombre = column.Name.ToUpper();
+                string tipo = column.PropertyType.Name;
+                if (nombre != "CUSTOMER" + "ID" && tipo != "ICollection`1")
+                    c.Add(column);
+            }
+            return c;
+        }
         public Customer()
         {
             Orders = new HashSet<Order>();
